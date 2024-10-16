2ff1399eeb90f37e52f3ebde86fb7aa1b4cb7bb0
marinagumeniuc <marina.gumeniuk.ani@gmail.com>
2024-10-16 12:11:58 +0300

    Final project setup and documentation

--- src/main/java/com/example/Expense_tracker/ExpenseTrackerApplication.java
+++ src/main/java/com/example/Expense_tracker/ExpenseTrackerApplication.java
@@ -1,8 +1,16 @@
 package com.example.Expense_tracker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+			        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Expense Tracker API",
+                description = "Expense Tracker API",
+                version = "1.0.0"
+        )
+)
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
